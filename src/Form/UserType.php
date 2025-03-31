952c5e35d2cf4dbaa2ccb981e9bda032cc9a5d6f
chedighribi <chady.ghribi.gc@gmail.com>
2025-03-31 16:22:59 +0200

    fix: add campus to user detail and form

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -18,6 +20,13 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('campus', EntityType::class, [
+                'label' => 'Campus',
+                'class' => Campus::class,
+                'choice_label' => 'nom',
+                'disabled' => true,
+                'mapped' => false,
+            ])
             ->add('email', EmailType::class)
             ->add('nom', TextType::class)
             ->add('prenom', TextType::class)
