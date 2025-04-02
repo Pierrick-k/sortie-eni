756dcfd657d3cab3c2bf0c2df91ee1a306d0e2c1
ValentinG <valentin.godec2024@campus-eni.fr>
2025-04-02 09:45:04 +0200

    feat: Ajout form sortie

---
+++ src/Form/SortieType.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Campus;
+use App\Entity\Lieu;
+use App\Entity\Sortie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom',TextType::class,[
+                'label'=>'Nom de la sortie',
+            ])
+            ->add('dateHeureDebut', DateTimeType::class,[
+                'label'=>'Date et heure de la sortie',
+                'widget' => 'single_text'])
+            ->setMethod('GET')
+            ->add('dateLimiteInscription', DateType::class,[
+                'label'=>'Date limite d\'inscription',
+                'widget' => 'single_text'])
+            ->add('nbInscriptionMax', IntegerType::class,[
+                'label'=>'Nombre de places',
+                'attr'=>[
+                    'min'=>1,
+                    'required'=>true,
+                ]
+            ])
+            ->add('duree', IntegerType::class,[
+                'label'=>'Durée (en minutes)',
+                'attr'=>[
+                    'min'=>1,
+                    'max'=>1440,
+                ]
+            ])
+            ->add('campus', EntityType::class, [
+                'label'=>'Campus',
+                'class' => Campus::class,
+                'choice_label' => 'nom',
+            ])
+            ->add('lieu', EntityType::class, [
+                'label'=>'Lieu',
+                'class' => Lieu::class,
+                'choice_label' => 'nom',
+            ])
+            ->add('infosSortie', TextAreaType::class,[
+                'label'=>'Description et infos',
+                'required'=>false,
+                ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
